4499ffd73efd5097e7b6300bb286075e1a81b570
Senthilmurugan Krishnan <sen.anju5@outlook.com>
2024-09-21 14:04:55 -0500

    Added package structure for MVVM clean

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.weatherapp
+package com.example.weatherapp.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
